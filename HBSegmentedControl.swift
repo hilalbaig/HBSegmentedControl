63512f32a05857b23d514695e4341c9551160d4e
Hilal Baig <hbaig@fusionsd.com.au>
2017-08-13 13:10:00 +0400

    HBSegmentedControl added

---
+++ HBSegmentedControl.swift
@@ -0,0 +1,225 @@
+//
+//  HBSegmentedControl.swift
+//  HBSegmentedControl
+//
+//  Created by Hilal Baig on 8/13/17.
+//  Copyright © 2017 Hilal Baig. All rights reserved.
+//
+
+
+import UIKit
+
+@IBDesignable class HBSegmentedControl: UIControl {
+    
+    fileprivate var labels = [UILabel]()
+    var thumbView = UIView()
+    
+    var items: [String] = ["Item 1", "Item 2", "Item 3"] {
+        didSet {
+            setupLabels()
+        }
+    }
+    
+    var selectedIndex : Int = 0 {
+        didSet {
+            displayNewSelectedIndex()
+        }
+    }
+    
+    @IBInspectable var selectedLabelColor : UIColor = UIColor.black {
+        didSet {
+            setSelectedColors()
+        }
+    }
+    
+    @IBInspectable var unselectedLabelColor : UIColor = UIColor.white {
+        didSet {
+            setSelectedColors()
+        }
+    }
+    
+    @IBInspectable var thumbColor : UIColor = UIColor.white {
+        didSet {
+            setSelectedColors()
+        }
+    }
+    
+    @IBInspectable var borderColor : UIColor = UIColor.white {
+        didSet {
+            layer.borderColor = borderColor.cgColor
+        }
+    }
+    
+    @IBInspectable var font : UIFont! = UIFont.systemFont(ofSize: 12) {
+        didSet {
+            setFont()
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupView()
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)!
+        setupView()
+    }
+    
+    func setupView(){
+        
+        layer.cornerRadius = frame.height / 2
+        layer.borderColor = UIColor(white: 1.0, alpha: 0.5).cgColor
+        layer.borderWidth = 2
+        
+        backgroundColor = UIColor.clear
+        
+        setupLabels()
+        
+        addIndividualItemConstraints(labels, mainView: self, padding: 0)
+        
+        insertSubview(thumbView, at: 0)
+    }
+    
+    func setupLabels(){
+        
+        for label in labels {
+            label.removeFromSuperview()
+        }
+        
+        labels.removeAll(keepingCapacity: true)
+        
+        for index in 1...items.count {
+            
+            let label = UILabel(frame: CGRect(x: 0, y: 0, width: 70, height: 40))
+            label.text = items[index - 1]
+            label.backgroundColor = UIColor.clear
+            label.textAlignment = .center
+            label.font = UIFont(name: "Avenir-Black", size: 15)
+            label.textColor = index == 1 ? selectedLabelColor : unselectedLabelColor
+            label.translatesAutoresizingMaskIntoConstraints = false
+
+            self.addSubview(label)
+            labels.append(label)
+        }
+        
+        addIndividualItemConstraints(labels, mainView: self, padding: 0)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        var selectFrame = self.bounds
+        let newWidth = selectFrame.width / CGFloat(items.count)
+        selectFrame.size.width = newWidth
+        thumbView.frame = selectFrame
+        thumbView.backgroundColor = thumbColor
+        thumbView.layer.cornerRadius = thumbView.frame.height / 2
+        
+        displayNewSelectedIndex()
+        
+    }
+    
+    override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        
+        let location = touch.location(in: self)
+        
+        var calculatedIndex : Int?
+        for (index, item) in labels.enumerated() {
+            if item.frame.contains(location) {
+                calculatedIndex = index
+            }
+        }
+        
+        
+        if calculatedIndex != nil {
+            selectedIndex = calculatedIndex!
+            sendActions(for: .valueChanged)
+        }
+        
+        return false
+    }
+    
+    func displayNewSelectedIndex(){
+        for (_, item) in labels.enumerated() {
+            item.textColor = unselectedLabelColor
+        }
+        
+        let label = labels[selectedIndex]
+        label.textColor = selectedLabelColor
+        
+
+        
+        
+        UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.8, animations: {
+            
+            self.thumbView.frame = label.frame
+            
+            }, completion: nil)
+        
+    }
+    
+    func addIndividualItemConstraints(_ items: [UIView], mainView: UIView, padding: CGFloat) {
+        
+        _ = mainView.constraints
+
+        for (index, button) in items.enumerated() {
+    
+            let topConstraint = NSLayoutConstraint(item: button, attribute: NSLayoutAttribute.top, relatedBy: NSLayoutRelation.equal, toItem: mainView, attribute: NSLayoutAttribute.top, multiplier: 1.0, constant: 0)
+            
+            let bottomConstraint = NSLayoutConstraint(item: button, attribute: NSLayoutAttribute.bottom, relatedBy: NSLayoutRelation.equal, toItem: mainView, attribute: NSLayoutAttribute.bottom, multiplier: 1.0, constant: 0)
+            
+            var rightConstraint : NSLayoutConstraint!
+            
+            if index == items.count - 1 {
+                
+                rightConstraint = NSLayoutConstraint(item: button, attribute: NSLayoutAttribute.right, relatedBy: NSLayoutRelation.equal, toItem: mainView, attribute: NSLayoutAttribute.right, multiplier: 1.0, constant: -padding)
+                
+            }else{
+                
+                let nextButton = items[index+1]
+                rightConstraint = NSLayoutConstraint(item: button, attribute: NSLayoutAttribute.right, relatedBy: NSLayoutRelation.equal, toItem: nextButton, attribute: NSLayoutAttribute.left, multiplier: 1.0, constant: -padding)
+            }
+            
+            
+            var leftConstraint : NSLayoutConstraint!
+            
+            if index == 0 {
+                
+                leftConstraint = NSLayoutConstraint(item: button, attribute: NSLayoutAttribute.left, relatedBy: NSLayoutRelation.equal, toItem: mainView, attribute: NSLayoutAttribute.left, multiplier: 1.0, constant: padding)
+                
+            }else{
+                
+                let prevButton = items[index-1]
+                leftConstraint = NSLayoutConstraint(item: button, attribute: NSLayoutAttribute.left, relatedBy: NSLayoutRelation.equal, toItem: prevButton, attribute: NSLayoutAttribute.right, multiplier: 1.0, constant: padding)
+                
+                let firstItem = items[0]
+                
+                let widthConstraint = NSLayoutConstraint(item: button, attribute: .width, relatedBy: NSLayoutRelation.equal, toItem: firstItem, attribute: .width, multiplier: 1.0  , constant: 0)
+                
+                mainView.addConstraint(widthConstraint)
+            }
+            
+            mainView.addConstraints([topConstraint, bottomConstraint, rightConstraint, leftConstraint])
+        }
+    }
+    
+    func setSelectedColors(){
+        for item in labels {
+            item.textColor = unselectedLabelColor
+        }
+        
+        if labels.count > 0 {
+            labels[0].textColor = selectedLabelColor
+        }
+        
+        thumbView.backgroundColor = thumbColor
+    }
+    
+    func setFont(){
+        for item in labels {
+            item.font = font
+        }
+    }
+}
--- HBSegmentedControl/ViewController.swift
+++ HBSegmentedControl/ViewController.swift
@@ -6,20 +6,63 @@
 //  Copyright © 2017 Hilal Baig. All rights reserved.
 //
 
+
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var titleLabel : UILabel!
+    @IBOutlet var topImageView : UIImageView!
+    @IBOutlet var salesLabel : UILabel!
+    @IBOutlet var salesValue : UILabel!
+    
+    @IBOutlet var segmentControl : HBSegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        titleLabel.font = UIFont(name: "Avenir-Book", size: 21)
+        titleLabel.textColor = UIColor.white
+        titleLabel.text = "Statistics"
+        
+        salesLabel.font = UIFont(name: "Avenir-Black", size: 16)
+        salesLabel.textColor = UIColor(red: 0.27, green: 0.80, blue: 0.34, alpha: 1.0)
+        salesLabel.text = "SALES"
+        
+        salesValue.font = UIFont(name: "Avenir-Black", size: 30)
+        salesValue.textColor = UIColor.white
+        salesValue.text = "$81,694"
+        
+        topImageView.image = UIImage(named: "background")
+        topImageView.clipsToBounds = true
+        topImageView.contentMode = .scaleAspectFill
+        
+        segmentControl.items = ["WEEKLY", "MONTHLY", "YEARLY"]
+        segmentControl.font = UIFont(name: "Avenir-Black", size: 12)
+        segmentControl.borderColor = UIColor(white: 1.0, alpha: 0.3)
+        segmentControl.selectedIndex = 1
+        segmentControl.addTarget(self, action: #selector(ViewController.segmentValueChanged(_:)), for: .valueChanged)
     }
-
+    
+    func segmentValueChanged(_ sender: AnyObject?){
+        
+        if segmentControl.selectedIndex == 0 {
+            salesValue.text = "$23,399"
+        }else if segmentControl.selectedIndex == 1{
+            salesValue.text = "$81,295"
+        }else{
+            salesValue.text = "$199,392"
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    override var preferredStatusBarStyle : UIStatusBarStyle {
+        return .lightContent
+    }
 }
 
