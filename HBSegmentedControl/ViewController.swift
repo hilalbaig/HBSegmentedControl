1a819b0e76f93ee71b416867a450551c047124cb
Hilal Baig <hbaig@fusionsd.com.au>
2017-08-13 12:34:18 +0400

    Initial Commit

---
+++ HBSegmentedControl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HBSegmentedControl
+//
+//  Created by Hilal Baig on 8/13/17.
+//  Copyright © 2017 Hilal Baig. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
